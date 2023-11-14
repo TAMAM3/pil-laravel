3b3e82011c2f9a85ae6a4230367068f93dc72834
TAMAM3 <tamamhabib123@gmail.com>
2023-11-14 22:26:56 +0700

    Push pertama saya untuk project laravel kedalam github

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+    <h1>Ini Adalah Halaman Home </h1>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/about">about</a></li>
+        <li><a href="/mahasiswa">mahasiswa</a></li>
+        <li><a href="/profile">profile</a></li>
+    </ul>
+</body>
+</html>
